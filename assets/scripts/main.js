98238028671e1361961ba967c51505974e82396c
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2015-09-25 16:56:24 +0400

    added work section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,33 @@
-function scroll(scrollLink, scrollDiv){
-  $(scrollLink).click(function (){
-    $('html, body').animate({
-        scrollTop: $(scrollDiv).offset().top
-    }, 500);
-  });
-};
-
-$(document).ready(function (){
+$(function (){
+  //scroll functionality
   scroll('#about-us-link', '#about-us');
   scroll('#services-link', '#services');
   scroll('#work-link', '#work');
   scroll('#team-link', '#team');
   scroll('#contact-link', '#contact');
+
+  //case stuidies functionality
+  var unslider = $('.case-studies').unslider({
+    speed: 400,
+  	delay: false,
+  	keys: true,
+  	dots: false,
+  	fluid: false
+  });
+
+
+  $('.unslider-arrow').on('click', function(e) {
+    var fn = this.className.split(' ')[1];
+
+    e.preventDefault();
+    unslider.data('unslider')[fn]();
+  });
 });
+
+function scroll(link, div){
+  $(link).on('click', function (){
+    $('html, body').animate({
+        scrollTop: $(div).offset().top
+    }, 500);
+  });
+};
